a21ecb353485cdc9c647e703047aed9f3d7574fe
Guilherme Moreira Santos <gmoreirasantos1325@hotmail.com>
2019-03-04 00:20:41 -0300

    Add basic notes regarding commens

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,26 @@
+// Comentario single-line
+
+
+/*
+  Comentario
+  de
+  multiplas
+  linhas
+*/
+
+/*
+  * Há
+  * Vários
+  * Estilos
+  * Adotados
+  * Pela
+  * Comunidade
+  * Para
+  * Aplicar
+  * Comentários
+*/
+
+// Evite comentários desnecessároios
+// Evite comentários longos
+// Evite comentários confusos
+// Evite comentários. Se você precisa comentar, você precisa limpar o código
