6e2ddeae42a97cfa8c40e5da1500105a471de252
Guilherme Moreira Santos <gmoreirasantos1325@hotmail.com>
2019-03-04 00:09:01 -0300

    Add first example - basic syntax elements of the language

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,14 @@
+// Javascript é separado por alguma estruturas sintáticas básixas, ex:
+
+// Senteças
+console.log('teste') // Isso é uma sentença, a unidade mais básica da sintaxe do js
+
+
+
+// Outro exemplo de construto sintático é o bloco, que serve para agrupar sentenças e/ou outros blocos dentro do código.
+// Funções, classes, estruturas de controle (if, else, case, for, while, etc) possuem um bloco associado a sua execução.
+// Um bloco é um espaço de codigo delimitado por um par de chaves '{}'. O exemplo abaixo é um exemplo de Javascript
+  // válido, já que nenhuma regra da versão atual da linguagem está sendo desrespeitada
+{
+  console.log('eu estou em um bloco')
+}
