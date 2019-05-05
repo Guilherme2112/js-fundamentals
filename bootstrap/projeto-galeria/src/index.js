c391dec482790baa8fbb87a9f6ac93b576f9d8f8
Guilherme Moreira Santos <gmoreirasantos1325@hotmail.com>
2019-05-05 15:35:36 -0300

    Add bootstrap project using webpack to bundle assets

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,11 @@
+// Sass pipeline
+import './scss/index.scss'
+
+
+// Js dependencies
+import 'jquery'
+import 'bootstrap'
+
+// Custom js
+import './js/core/includes'
+import './js/plugins/cityButtons'
